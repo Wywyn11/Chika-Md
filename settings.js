7491a4ec24d9c372431f98b5be291af669d40fb0
Wywyn11 <112207957+Wywyn11@users.noreply.github.com>
2022-11-28 00:57:47 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @WYN
+› Base Ori @Wynnn
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -22,34 +22,34 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6289522915426','6289522915426','6289522915426'] //6289522915426'
+global.premium = ['6289522915426'] //6289522915426 ' 
+global.ownernomer = '6289522915426' //6289522915426 '
 
-//—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+//—————「 Winky Ofc & Wyn Bot 」—————//
+global.ownername = 'Wyn' //
+global.botname = 'Wyn Ofc' //
+global.footer = 'Wyn' //
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
-//ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
+//ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), 
 global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
+global.ig = 'https://instagram.com/Wynxxnxx'
 global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.myweb = 'https://s.id/winkybot'
+global.email = 'winkyyemel@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Winky ofc' //
+global.author = 'Wyn' //
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
 global.sessionName = 'session'
 
 //—————「 Set Limit 」—————//
-//terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
+//terserah mau ubah atau nggak, note : 
 global.limitawal = {
 premium: "Infinity",
 free: 15,
